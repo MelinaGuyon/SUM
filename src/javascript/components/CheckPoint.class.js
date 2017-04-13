74cb496371d9baf5423d6b8ff265e2313a3dc214
MelinaGuyon <guyonmelina@gmail.com>
2017-04-13 15:23:46 +0200

    Put ratio vertical in storage

--- src/javascript/components/CheckPoint.class.js
+++ src/javascript/components/CheckPoint.class.js
@@ -9,18 +9,17 @@ class CheckPoint {
       this.blackboard = options.blackboard
       this.blackBoardIndex = options.blackBoardIndex
       this.index = options.index
-      this.ratioVertical = options.ratioVertical
 
       this.init()
       this.bind()
     }
 
     init() {
-      this.checkPoint.beginFill(0xffffff, 1)
+      this.checkPoint.beginFill(0xffffff, 0)
       this.checkPoint.drawCircle(0, 0, datas.datasBlackboards[this.blackBoardIndex].checkPoints[this.index].rayon)
       this.checkPoint.endFill()
-      this.checkPoint.x = datas.datasBlackboards[this.blackBoardIndex].checkPoints[this.index].x * this.ratioVertical + this.blackboard.graphicsData[0].shape.x
-      this.checkPoint.y = datas.datasBlackboards[this.blackBoardIndex].checkPoints[this.index].y * this.ratioVertical + this.blackboard.graphicsData[0].shape.y
+      this.checkPoint.x = datas.datasBlackboards[this.blackBoardIndex].checkPoints[this.index].x * STORAGE.ratioVertical + this.blackboard.graphicsData[0].shape.x
+      this.checkPoint.y = datas.datasBlackboards[this.blackBoardIndex].checkPoints[this.index].y * STORAGE.ratioVertical + this.blackboard.graphicsData[0].shape.y
       this.checkPoint.interactive = true // pour attribuer événements à this.checkPoint
       this.checkPoint.isChecked = false
 
@@ -53,7 +52,7 @@ class CheckPoint {
         })
 
         if (this.blackboard.isTestLaunch) {
-          let ImageDeformationClass = new ImageDeformation({ image : 'assets/deformation.png' })
+          let ImageDeformationClass = new ImageDeformation({ image : 'assets/carousel-3.jpg' })
           ImageDeformationClass.animate(ImageDeformationClass)
         }
       }

6d4924ae6c087e9f2da1ec7af098e88d58170e79
MelinaGuyon <guyonmelina@gmail.com>
2017-05-11 12:29:00 +0200

    Add voice over on first challenge

--- src/javascript/datas/soundBank.js
+++ src/javascript/datas/soundBank.js
@@ -5,8 +5,11 @@ const soundBank = {
   firstChallenge : [
     ['assets/sounds/first-challenge/gouttes.mp3', 'assets/sounds/first-challenge/feu.mp3'],
     ['assets/sounds/first-challenge/oiseaux.mp3', 'assets/sounds/first-challenge/montagne.mp3'],
-    ['assets/sounds/first-challenge/gouttes.mp3', 'assets/sounds/first-challenge/feu.mp3']
-  ]
+    ['assets/sounds/first-challenge/gouttes.mp3', 'assets/sounds/first-challenge/feu.mp3'],
+  ],
+  voiceOver : {
+    firstChallenge: 'assets/sounds/first-challenge/voice.wav'
+  }
 }
 
 export default soundBank

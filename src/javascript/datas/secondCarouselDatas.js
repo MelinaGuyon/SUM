67590ae665fbd992833bc08da77d3942f750a389
MelinaGuyon <guyonmelina@gmail.com>
2017-06-02 15:50:50 +0200

    Add secondCarouel

--- src/javascript/datas/secondCarouselDatas.js
+++ src/javascript/datas/secondCarouselDatas.js
@@ -1,24 +1,16 @@
 const secondCarouselDatas = {
   datasImages: [
     'assets/before-challenge-2/carousel-fond.jpg',
-    'assets/before-challenge-2/0-forme-1.png',
-    'assets/before-challenge-2/0-forme-2.png',
-    'assets/before-challenge-2/0-forme-3.png',
-    'assets/before-challenge-2/0-forme-4.png',
-    'assets/before-challenge-2/0-forme-5.gif',
-    'assets/before-challenge-2/0-forme-6.png',
     'assets/before-challenge-2/1-forme-1.png',
     'assets/before-challenge-2/1-forme-2.png',
+    'assets/before-challenge-2/1-forme-3.png',
     'assets/before-challenge-2/2-forme-1.png',
     'assets/before-challenge-2/2-forme-2.png',
     'assets/before-challenge-2/2-forme-3.png',
     'assets/before-challenge-2/3-forme-1.png',
-    'assets/before-challenge-2/3-forme-2.png',
     'assets/before-challenge-2/4-forme-1.png',
     'assets/before-challenge-2/4-forme-2.png',
-    'assets/before-challenge-2/5-forme-1.png',
-    'assets/before-challenge-2/5-forme-2.png',
-    'assets/before-challenge-2/5-forme-3.png'
+    'assets/before-challenge-2/5-forme-1.png'
   ],
   datasEyes: [
     {
